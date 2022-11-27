02ceefd7d3ad0022fd5f33026dd4bdb866b00461
AdilMS2019 <98103227+AdilMS2019@users.noreply.github.com>
2022-11-27 00:09:44 +0000

    Remove useless files

--- Technical Reports/Report 1 Hello World/Hello World Report.md
+++ Technical Reports/Report 1 Hello World/Hello World Report.md
@@ -2,7 +2,7 @@ Technologies:
 NodeJS frontend
 begin by making directory
 ExpressJS backend
-npm install -g express-generator - installs expressjs
+npm install express - installs expressjs
 express helloWorld --view pug - creates expressjs server
 cd helloWorld
 npm install - install application dependencies
--- Technical Reports/Report 1 Hello World/helloWorld/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const path = require("path");
-const app = express();
-app.use(express.static(path.join(__dirname, "public")));
-
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "public/index.html"));
-});
-app.listen(8000, () => console.log("Server is running on Port 8000"));
