152550877b38c79ad30f3831564b64e52724d907
AdilMS2019 <98103227+AdilMS2019@users.noreply.github.com>
2022-12-03 21:27:05 +0000

    Add JSDoc to status.js

--- Technical Reports/Report 1 Hello World/hello-world/public/js/status.js
+++ Technical Reports/Report 1 Hello World/hello-world/public/js/status.js
@@ -1,5 +1,13 @@
 document.addEventListener('DOMContentLoaded', init, false);
 
+/**
+ * Checks whether the website is online or offline and changes element in index.html
+ * to mirror this.
+ * 
+ * @author Diogo Spinola
+ * @see https://blog.logrocket.com/how-to-build-a-progressive-web-app-pwa-with-node-js/
+ * 
+ */
 function init() {
     if (!navigator.onLine) {
         const statusElem = document.querySelector('.page-status')
