4da4028e8a594edda0c407932c92bc6d9a1ace32
AdilMS2019 <98103227+AdilMS2019@users.noreply.github.com>
2023-03-09 14:27:22 +0000

    Create home page file

--- Project/App/app.js
+++ Project/App/app.js
@@ -4,10 +4,16 @@ const https = require('https')
 const app = express();
 const port = 3000;
 
-app.get('/fyp', (req, res) => {
-  res.send('Hello World!');
+const path = require('path')
+
+app.get('/fyp/home', (req, res) => {
+  res.sendFile(path.join(__dirname,'lib/home.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+app.listen(port, (error) =>{
+  if(!error)
+      console.log("Server is Successfully Running, and App is listening on port "+ port)
+  else 
+      console.log("Error occurred, server can't start", error);
+  }
+);
