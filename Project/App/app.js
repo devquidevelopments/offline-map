ff10cddd8d908a8dc5e521a1d384305fdbab2dd6
AdilMS2019 <98103227+AdilMS2019@users.noreply.github.com>
2023-03-09 19:24:21 +0000

    Create navbar

--- Project/App/app.js
+++ Project/App/app.js
@@ -1,19 +1,19 @@
 const express = require('express');
-const http = require('http')
-const https = require('https')
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const path = require('path')
 
-app.get('/fyp/home', (req, res) => {
-  res.sendFile(path.join(__dirname,'lib/home.html'));
+app.use(express.static(path.join(__dirname, '/public')))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/home.html'));
 });
 
-app.listen(port, (error) =>{
-  if(!error)
-      console.log("Server is Successfully Running, and App is listening on port "+ port)
-  else 
-      console.log("Error occurred, server can't start", error);
-  }
+app.listen(port, (error) => {
+  if (!error)
+    console.log("Server is Successfully Running, and App is listening on port " + port)
+  else
+    console.log("Error occurred, server can't start", error);
+}
 );
