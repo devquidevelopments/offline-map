a4a2b75e343b3623dad23957d886856d27cc6700
AdilMS2019 <98103227+AdilMS2019@users.noreply.github.com>
2023-03-17 17:41:36 +0000

    Complete basic search function

--- Project/App/public/scripts/initMap.js
+++ Project/App/public/scripts/initMap.js
@@ -17,6 +17,22 @@ function initMap() {
         maxZoom: 18,
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
+
+    var geocoder = L.Control.geocoder({
+        defaultMarkGeocode: false
+    })
+        .on('markgeocode', function (e) {
+            var bbox = e.geocode.bbox;
+            var poly = L.polygon([
+                bbox.getSouthEast(),
+                bbox.getNorthEast(),
+                bbox.getNorthWest(),
+                bbox.getSouthWest()
+            ]).addTo(map);
+            map.fitBounds(poly.getBounds());
+        })
+        .addTo(map);
+
     // map.locate({
     //     setView: true,
     //     watch: true,
