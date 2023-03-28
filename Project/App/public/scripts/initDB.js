9caba17b6c37cc57f2ed17a1fe43213ec1b63b61
AdilMS2019 <98103227+AdilMS2019@users.noreply.github.com>
2023-03-28 20:14:57 +0100

    Update db

--- Project/App/public/scripts/initDB.js
+++ Project/App/public/scripts/initDB.js
@@ -1,13 +1,15 @@
-let request = indexedDB.open('localDB', 1);
-request.onerror = function (event) {
-    console.log("Database error: " + event.target.errorCode);
-};
+let request = indexedDB.open('mapTiles', 1);
+
 request.onsuccess = function (event) {
     db = event.target.result;
     console.log("Database opened successfully");
 };
+
 request.onupgradeneeded = function (event) {
-    let db = event.target.result;
-    let objectStore = db.createObjectStore("myObjectStore", { keyPath: "id" });
-    objectStore.createIndex("name", "name", { unique: false });
+    const db = event.target.result;
+    const objectStore = db.createObjectStore("tile", { keyPath: "url" });
+};
+
+request.onerror = function (event) {
+    console.log("Database error: " + event.target.errorCode);
 };
