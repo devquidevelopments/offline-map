d86d3d844386dd8cb0011901b18aceac5fd167e8
AdilMS2019 <98103227+AdilMS2019@users.noreply.github.com>
2023-03-28 17:52:52 +0100

    Create base for overpass test

---
+++ Project/OfflineApp/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const http = require('http');
+const overpassFrontend = require('overpass-frontend');
+const path = require('path')
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, '/public')))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/home.html'));
+});
+
+app.listen(port, (error) => {
+    if (!error)
+        console.log("Server is Successfully Running, and App is listening on port " + port)
+    else
+        console.log("Error occurred, server can't start", error);
+}
+);
---
+++ Project/OfflineApp/public/sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'fyp-cache';
+const toCache = [
+    '/',
+    '/home.html',
+    '/res/dark-mode.png',
+    '/res/globe-192.png',
+    '/res/globe-512.png',
+    '/res/globe-vector.svg',
+    '/scripts/fyp.js',
+    '/scripts/fyp.webmanifest',
+    '/scripts/initDB.js'
+];
+
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                return cache.addAll(toCache)
+            })
+            .then(self.skipWaiting())
+    )
+})
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        fetch(event.request)
+            .catch(() => {
+                return caches.open(CACHE_NAME)
+                    .then((cache) => {
+                        return cache.match(event.request)
+                    })
+            })
+    )
+})
+
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys()
+            .then((keyList) => {
+                return Promise.all(keyList.map((key) => {
+                    if (key !== CACHE_NAME) {
+                        console.log('[ServiceWorker] Removing old cache', key)
+                        return caches.delete(key)
+                    }
+                }))
+            })
+            .then(() => self.clients.claim())
+    )
+})
