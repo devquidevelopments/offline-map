9130d753ed5584fdf71b05b031b25f358483adc5
AdilMS2019 <98103227+AdilMS2019@users.noreply.github.com>
2023-04-01 06:00:15 +0100

    Comment app.js

--- App/app.js
+++ App/app.js
@@ -1,16 +1,20 @@
+// Imports necessary libraries for the server
 const express = require('express');
-const http = require('http');
+const path = require('path')
+
+// Creates an express app object and defines the port at which it will be hosted.
 const app = express();
 const port = process.env.PORT || 3000;
 
-const path = require('path')
-
+// Mounts app at the public folder in the app directory
 app.use(express.static(path.join(__dirname, '/public')))
 
+// Sets home.html as the landing page for the application
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/home.html'));
 });
 
+// Starts the server, declaring the port at which it is hosted.
 app.listen(port, (error) => {
   if (!error)
     console.log("Server is Successfully Running, and App is listening on port " + port)
